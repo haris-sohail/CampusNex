8959c334548d0a3b8756854cc9f7c7e742bdc86b
Kalsoom Tariq <i212487@nu.edu.pk>
2024-04-17 03:56:49 +0500

    Login Issue Resolved Hallelujahhh

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace CampusNex.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap close {
+            get {
+                object obj = ResourceManager.GetObject("close", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap searchIcon {
             get {
                 object obj = ResourceManager.GetObject("searchIcon", resourceCulture);
